a796ca633814ed11a89b7142114a1f7fa6ffd632
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-05-26 17:08:26 +0100

    Planned queries

---
+++ db/queries.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once 'dbfunctions.php';
+
+// Get current status of controls
+function getControls()
+
+// Turn (off/on) a (specified control) - heating, lighting, water, windows
+function setControl($control, $value) {
+
+}
+
+// Get current/30day (make the 30 day per day not time in last 30 days) humidity/temperature data - in one using nested (temp, humid, temp[], humid[])?
+// No - should be separated
+function getOutsideTemperature() {
+
+}
+
+function getInsideTemperature() {
+
+}
+
+function getPlantTemperature($plant) {
+
+}
+
+// get average humidity across all plants
+function getHumidity() {
+
+}
+
+function getPlantHumidity($plant) {
+
+}
+
+// Change control mode (mode)
+function changeMode($newMode) {
+    
+}
