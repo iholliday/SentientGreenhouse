f7105a87d3fd1231b31d6057e0e9315388ea368b
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-05-26 11:07:46 +0100

    Test

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                             <div class="sb-sidenav-menu-heading">Core</div>
                             <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
+                                Dashboardtest
                             </a>
                              <a class="nav-link" href="settings.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
