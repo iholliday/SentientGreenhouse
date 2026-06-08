607f3bacab02fd59d6e1192f20027efd4e7bf09b
Ivy Holliday <73825078+iholliday@users.noreply.github.com>
2026-06-08 20:51:24 +0100

    Controls page start

---
+++ php/controls.php
@@ -0,0 +1,21 @@
+<?php
+//controls output at the top - row of icon circles in columns
+
+//form for changing hlww configuration with checkboxes and save to update - changes mode to custom (before changing output config)
+
+//current mode circle, form for changing mode, save - updates table, mode have their own effects in nodered - auto, secure (auto but closed windows), custom (manual)
+
+?>
+
+<div>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+    <form action="outputChange.php" method="POST">
+        
+    </form>
+    <form action="modeChange.php" method="POST"></form>
+</div>
